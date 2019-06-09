a82b88e7e1b69d5792f3b233bda4a4aa56b04861
Your Name <your@example.com>
2019-06-09 16:08:26 +0800

    a

---
+++ 9.php
@@ -0,0 +1,26 @@
+<?php 
+
+
+
+$string = "Have you ever gone shopping and";
+$len = strlen($string);
+echo $len;die;
+$arr = [];
+for ($i=0; $i < $len; $i++) { 
+	if ($string[$i] == 1) {
+		$arr[$string] = 1
+	}else{
+		$string[$i]++
+	}
+	if ($arr[$string[$i]] >= 3) {
+		echo "11";
+	}
+}
+
+
+
+
+
+
+
+ ?>
