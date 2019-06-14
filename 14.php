0f3635ba3848a38e2d88af6dfb95e9f9941190d9
Your Name <your@example.com>
2019-06-14 15:12:13 +0800

    e

---
+++ 14.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+
+
+
+
+function aaa($n,$m,$number)
+{
+	// $data = '';
+	// for ($i=$n; $i < $m; $i++) { 
+	// 	if ($m%10 == 1) {
+	// 		$data = 1;
+	// 	}
+	// }
+	// return $data;
+	return 21%10;
+}
+
+echo aaa(1,300,1);
+
+
+
+
+ ?>
